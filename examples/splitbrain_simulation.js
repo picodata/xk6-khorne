6b0c91f725f2e281e1588d65a9677d53935d4e9a
dodokek <alex.morozov.dd@mail.ru>
2024-10-24 10:55:07 +0300

    Remove dead code, cosmetic changes

--- examples/splitbrain_simulation.js
+++ examples/splitbrain_simulation.js
@@ -2,7 +2,7 @@
 // We basically disable one slave node from storage replicaset
 // And hope that replicaset will return in consistent state
 
-import { fail } from 'k6';
+import k6 from 'k6';
 import khorne from "k6/x/khorne";
 
 export const options = Object.assign({}, {}, {
@@ -18,7 +18,7 @@ export default function (opts) {
   let result = khorne.CheckPodsHealth("test-namespace", ["storage-0-1"])
 
   if (result.success) {
-    fail("Master node became consistent, however new master has been elected. Splitbrain wasn't detected. " + result.error)
+    k6.fail("Master node became consistent, however new master has been elected. Splitbrain wasn't detected. " + result.error)
   } else {
     console.log("Splitbrain has been handled, old master status: " + result.error)
   }
